5ee16a7512afdba093cb88cf1ff74fe60ff6a8fa
Jr_Laptop <Jr@outlook.com>
2024-05-17 10:22:07 +0800

    修复监听消息处理错误的问题

--- src/main/java/AutoDraw.java
+++ src/main/java/AutoDraw.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -22,15 +21,17 @@ import java.util.concurrent.CompletionStage;
 
 public class AutoDraw {
     private static final ObjectMapper objectMapper = new ObjectMapper();
-    private static final String host = "://192.168.195.199:8188";
+    private static final String host = "://192.168.2.199:8188";
     private static final String clientId = "CustomJavaAPI";
 
     public static void main(String[] args) {
-//        autoDraw();
-        getQueueRemain();
+        autoDraw();
+//        getQueueRemain();
     }
 
     private static void autoDraw() {
+        pushTask();
+
         HttpClient client = HttpClient.newHttpClient();
         URI uri = URI.create("ws" + host + "/ws?client_id=" + clientId);
 
@@ -45,14 +46,25 @@ public class AutoDraw {
 
     static class WebSocketListener implements Listener {
         @Override
+        public void onOpen(WebSocket webSocket) {
+            System.out.println("WebSocket连接建立");
+            Listener.super.onOpen(webSocket);
+        }
+
+        @Override
+        public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
+            System.out.println("WebSocket连接关闭");
+            return Listener.super.onClose(webSocket, statusCode, reason);
+        }
+
+        @Override
         public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+            System.out.println(data.toString());
             try {
-                JsonNode jsonNode = objectMapper.readTree(data.toString());
-                if ("executing".equals(jsonNode.path("type").asText())) {
-                    JsonNode dataNode = jsonNode.path("data");
-                    if (dataNode.path("node").isNull()) {
-                        pushTask(data.toString());
-                    }
+                JsonNode rootNode = objectMapper.readTree(data.toString());
+                JsonNode queueRemainingNode = rootNode.path("data").path("status").path("exec_info").path("queue_remaining");
+                if (queueRemainingNode.asInt() == 0) {
+                    pushTask();
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -61,21 +73,19 @@ public class AutoDraw {
         }
     }
 
-    private static void pushTask(String message) {
-        System.out.println(message);
+    private static void pushTask() {
         String result;
 
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            JsonNode workflowNode = objectMapper.readTree(new File("C:\\Users\\suyis\\Downloads\\workflow_api.json"));
+            JsonNode workflowNode = objectMapper.readTree(
+                    new File("/home/jr/ComfyUI/workflow_api.json"));
 
             ObjectNode rootNode = objectMapper.createObjectNode();
             rootNode.put("client_id", clientId);
             rootNode.set("prompt", workflowNode);
 
-            // 将修改后的JsonNode转换回JSON字符串
             String jsonString = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(rootNode);
 
-            // Step 3: 使用Apache HttpClient发送POST请求
             StringEntity params = new StringEntity(jsonString, "utf-8");
 
             HttpPost httpPost = new HttpPost("http" + host + "/prompt");
@@ -83,14 +93,13 @@ public class AutoDraw {
             httpPost.setEntity(params);
 
             try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
-                // 处理响应
                 HttpEntity responseEntity = response.getEntity();
                 if (responseEntity != null) {
                     result = EntityUtils.toString(responseEntity, "utf-8");
                     Map<String, Object> map = objectMapper.readValue(result, new TypeReference<Map<String, Object>>() {
                     });
                     String promptId = (String) map.get("prompt_id");
-                    System.out.println(promptId);
+                    System.out.println("新建任务： " + promptId);
                 }
             }
         } catch (IOException e) {
@@ -98,32 +107,4 @@ public class AutoDraw {
         }
     }
 
-    private static void getQueueRemain() {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-
-        String result = "";
-        CloseableHttpResponse response = null;
-
-        try {
-            HttpGet httpGet = new HttpGet("http" + host + "/prompt");
-            response = httpClient.execute(httpGet);
-
-            //判断响应状态
-            if (response.getStatusLine().getStatusCode() == 200) {
-                result = EntityUtils.toString(response.getEntity(), "UTF-8");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (response != null) {
-                    response.close();
-                }
-                httpClient.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println(result);
-    }
 }

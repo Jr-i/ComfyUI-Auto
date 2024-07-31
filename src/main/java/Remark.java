2e45c80763e09ba4fa3128b00567862f2cab5716
Jr_Laptop <Jr@outlook.com>
2024-07-31 17:56:53 +0800

    加入是否横屏，加载配置文件名称这两个属性

--- src/main/java/Remark.java
+++ src/main/java/Remark.java
@@ -1,58 +1,25 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Remark {
+    // 当前加载的配置文件名称
+    private String configName;
+    // 图片是否为横屏
+    private boolean isLandscape;
+    // char.txt的特征码
     private String charFeature;
+    // location.txt的特征码
     private String locationFeature;
+    // char.txt的总行数
     private int charTotalLines;
+    // location.txt的总行数
     private int locationTotalLines;
+    // 当前char.txt的行数
     private int charIndex;
+    // 当前location.txt的行数
     private int locationIndex;
-
-    public String getCharFeature() {
-        return charFeature;
-    }
-
-    public void setCharFeature(String charFeature) {
-        this.charFeature = charFeature;
-    }
-
-    public String getLocationFeature() {
-        return locationFeature;
-    }
-
-    public void setLocationFeature(String locationFeature) {
-        this.locationFeature = locationFeature;
-    }
-
-    public int getCharTotalLines() {
-        return charTotalLines;
-    }
-
-    public void setCharTotalLines(int charTotalLines) {
-        this.charTotalLines = charTotalLines;
-    }
-
-    public int getLocationTotalLines() {
-        return locationTotalLines;
-    }
-
-    public void setLocationTotalLines(int locationTotalLines) {
-        this.locationTotalLines = locationTotalLines;
-    }
-
-    public int getCharIndex() {
-        return charIndex;
-    }
-
-    public void setCharIndex(int charIndex) {
-        this.charIndex = charIndex;
-    }
-
-    public int getLocationIndex() {
-        return locationIndex;
-    }
-
-    public void setLocationIndex(int locationIndex) {
-        this.locationIndex = locationIndex;
-    }
 
     public void incrementCharIndex() {
         this.charIndex++;
